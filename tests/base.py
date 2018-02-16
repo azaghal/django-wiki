aa092eb7f72c568b9050eb750b16b145e1ee04dc
Branko Majic <branko@majic.rs>
2018-02-16 15:54:02 +0100

    Update base test classes to include regular users in test data creation:
    - Introduce small helper namedtuple for more easily keeping track of
      user information.
    - Add a convenience method for logging-in as specific user, supporting
      both the admin and regular user login.
    - Introduce an additional test exception to be used by testing code
      itself (for validating use of internal test utilities).
    - Introduce additional mixin for creating regular users as part of
      test data, and add it to base test classes.
    - Each user can be created with specific set of privileges if
      desired. Currently just create three separate users for future use.

--- tests/base.py
+++ tests/base.py
@@ -1,24 +1,71 @@
 import os
 import unittest
+from collections import namedtuple
 
 import django_functest
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from django.core.exceptions import PermissionDenied
 from django.template import Context, Template
 from django.test import TestCase, override_settings
 from django.urls import reverse
 from wiki.models import URLPath
 
+User = get_user_model()
+
+TestUserInfo = namedtuple('UserInfo', 'username password mail permissions')
+TestUserInfo.__doc__ = """ Helper structure for defining test user
+information. Keep in mind that that this structure does not reflect
+the database structure, nor is in any way tied-in into the database
+structure. Its sole purpose is to provide a convenient way to access
+information about test users without using the dict's [] operator.
+
+Available attributes:
+
+- username
+- password
+- mail
+- permissions (tuple of strings, where each string represents a
+  permission in Django admin system).
+
+Attributes can be accessed with dot-syntax, like any standard
+namedtuple. For example:
+
+    user_info = TestUserInfo('user1', 'user1secret', 'user1@example.com', ())
+    print(user_info.username)
+
+    user_info = TestUserInfo('user1', 'user1secret', 'user1@example.com', ('wiki.assign', 'wiki.moderate'))
+    print(user_info.permissions)
+"""
+
+
 SUPERUSER1_USERNAME = 'admin'
 SUPERUSER1_PASSWORD = 'secret'
 
 
-class RequireSuperuserMixin:
+REGULAR_USERS = {
+    'user1': TestUserInfo('user1', 'user1secret', 'user1@example.com', ()),
+    'user2': TestUserInfo('user2', 'user2secret', 'user2@example.com', ()),
+    'user3': TestUserInfo('user3', 'user3secret', 'user3@example.com', ()),
+}
 
+
+class TestException(Exception):
+    """
+    Exception raised internally by the test code itself in case the
+    test code mixins or samples are not used correctly. The only
+    purpose of this exception is to denote an error within the test
+    code abstractions, and it must not be used for regular test code
+    to mask-out any actual application-level exceptions.
+    """
+    pass
+
+
+class RequireSuperuserMixin:
     def setUp(self):
         super().setUp()
-
-        from django.contrib.auth import get_user_model
-        User = get_user_model()
 
         self.superuser1 = User.objects.create_superuser(
             SUPERUSER1_USERNAME,
@@ -27,7 +74,26 @@ class RequireSuperuserMixin:
         )
 
 
-class RequireBasicData(RequireSuperuserMixin):
+class RequireRegularUserMixin:
+
+    def setUp(self):
+        super(RequireRegularUserMixin, self).setUp()
+        self.users = {}
+
+        for user_id, user_info in REGULAR_USERS.items():
+            user = User.objects.create_user(
+                username=user_info.username,
+                password=user_info.password,
+                email=user_info.password,
+            )
+
+            for permission in user_info.permissions:
+                user.user_permissions.add(Permission.objects.get(codename=permission))
+
+            self.users[user_id] = user
+
+
+class RequireBasicData(RequireRegularUserMixin, RequireSuperuserMixin):
     """
     Mixin that creates common data required for all tests.
     """
@@ -61,7 +127,29 @@ class DjangoClientTestBase(TestBase):
     def setUp(self):
         super().setUp()
 
-        self.client.login(username=SUPERUSER1_USERNAME, password=SUPERUSER1_PASSWORD)
+        self.login_as_user(SUPERUSER1_USERNAME)
+
+    def login_as_user(self, user):
+        """
+        Logs-in the specified user. For list of users see the keys of
+        REGULAR_USERS dictionary.
+
+        If it is required to log-in as the administrator user, set
+        user to 'name'.
+        """
+
+        if user == 'admin':
+            username = 'admin'
+            password = 'secret'
+        else:
+            try:
+                username = REGULAR_USERS[user].username
+                password = REGULAR_USERS[user].password
+            except KeyError:
+                raise TestException("No such test user '%s', available users are: %s" % (user, ", ". join(REGULAR_USERS.keys())))
+
+        if not self.client.login(username=username, password=password):
+            raise PermissionDenied("Could not log-in as specified test-user '%s' using password '%s'" % (username, password))
 
 
 class WebTestCommonMixin(RequireBasicData, django_functest.ShortcutLoginMixin):
